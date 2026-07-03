63bfea43352f0628beed920a5159d316aa2e614c
IStealCode <88727697+IStealCode@users.noreply.github.com>
2026-07-03 11:11:17 +0200

    Update urls.py

--- dj_chat/home/urls.py
+++ dj_chat/home/urls.py
@@ -1,20 +1,20 @@
-from django.urls import path
+from django.urls import path  #importiere die project urls.py
 
-from .views import index_view
+from .views import index_view #importiere die views
 from .views import home_view
 
 urlpatterns = [
-        path("",
+        path("",                #wenn seite ohne pfad aufgerufen wird wird index angezeigt
              index_view,
              name="index",
         ),
         
-        path("index/",
+        path("index/",          #index pfad
              index_view,
              name="index",
         ),
 
-        path("home/",
+        path("home/",          #homepage pfad
              home_view,
              name="home",
         ),
