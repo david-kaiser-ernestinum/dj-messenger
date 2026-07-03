3564ace27b227da219b95d8165840cd1591010d9
IStealCode <88727697+IStealCode@users.noreply.github.com>
2026-07-03 11:16:26 +0200

    Update urls.py

--- dj_chat/accounts/urls.py
+++ dj_chat/accounts/urls.py
@@ -1,21 +1,21 @@
-from django.urls import path
+from django.urls import path #importiere die projekt urls.py
 
-from .views import register_view
+from .views import register_view #importiere views.py methoden
 from .views import login_view
 from .views import logout_view
 
 urlpatterns = [
-        path("register/",
+        path("register/",        #register seite
              register_view,
              name="register",
         ),
         
-        path("login/",
+        path("login/",           #login seite
              login_view,
              name="login",
         ),
 
-        path("logout/",
+        path("logout/",          #logout seite, die wieder auf /index.html weiterleitet
              logout_view,
              name="logout",
         ),
