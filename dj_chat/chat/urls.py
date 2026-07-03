f219cb3f4949755b55191c61d4b00680ac0db828
IStealCode <88727697+IStealCode@users.noreply.github.com>
2026-07-03 10:09:33 +0200

    Update urls.py

--- dj_chat/chat/urls.py
+++ dj_chat/chat/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from .views import create_chat, view_chat, send_message, add_user
 
 urlpatterns = [
-    path("create/", create_chat, name="create_chat"),
-    path("<int:chat_id>/", view_chat, name="view_chat"),
-    path("send_message/<int:chat_id>/", send_message, name="send_message"),
-    path("add_user/<int:chat_id>/", add_user, name="add_user"),
+    path("create/", create_chat, name="create_chat"), #URL-Pfad um ein Chat zu erstellen
+    path("<int:chat_id>/", view_chat, name="view_chat"), #URL-Pfad um den Chat zu sehen
+    path("send_message/<int:chat_id>/", send_message, name="send_message"), #URL-Pfad um eine Nachricht zu schicken
+    path("add_user/<int:chat_id>/", add_user, name="add_user"), #URL-Pfad um einen neuen Teilnehmer hinzuzufügen
 ]
 
